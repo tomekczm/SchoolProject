913f7ef1d014c0309d1bc24ae6645b6a90f73b4d
Mammon <malpa@gmail.com>
2026-09-18 09:59:03 +0200

    npc chodzi

--- src/entities/npc.ts
+++ src/entities/npc.ts
@@ -8,19 +8,25 @@ export function makeNPC(game: KAPLAYCtx) {
     let punkt = 0;
     const punkty = [
         new game.Vec2(80,400),
-        new game.Vec2(400,400)
+        new game.Vec2(400,400),
+        new game.Vec2(400,80),
+        new game.Vec2(80,80)
     ];
     const Npc = game.add([ 
         game.rect(32, 32),
         game.pos(80, 80),
-        game.color(0.5, 0.5, 1),
+        game.color(255, 105, 180),
     ]);
     Npc.onUpdate(() => {
         
         Npc.moveTo(punkty[punkt], speedNpc);
         if(Npc.pos.dist(punkty[punkt]) == 0  ){
-            punkt = 1;
+            punkt ++;
+            if(punkt == 4){
+                punkt = 0;
+            }
         }
+        
     });
     
 }
