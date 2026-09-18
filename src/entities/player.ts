2b46c44a6bc98a27b1221f1fe826d870d4fa764b
tomekcz <tom@tomekcz.com>
2026-09-18 10:49:43 +0200

    Add globals

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -6,7 +6,9 @@ export function addPlayer(game: KAPLAYCtx) {
     const player = game.add([
         game.rect(32, 32),
         game.area(),
-        game.pos(10, 20),
+        game.pos(0, 0),
+        game.rotate(0),
+        game.anchor("center"),
         "Player"
     ])
 
@@ -27,6 +29,7 @@ export function addPlayer(game: KAPLAYCtx) {
     })
 
     game.onUpdate(() => {
+        player.rotateTo(player.pos.angle(game.mousePos()))
         let motion = new game.Vec2(0,0)
         if(game.isKeyDown("d")) motion = motion.add(1, 0);
         if(game.isKeyDown("a")) motion = motion.add(-1, 0);
@@ -39,4 +42,5 @@ export function addPlayer(game: KAPLAYCtx) {
         player.move(motion)
         game.setCamPos(game.getCamPos().lerp(player.pos, 0.1))
     })
+    return player
 }
--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ game.loadSprite("bean", "sprites/bean.png");
 
 game.add([game.pos(120, 80), game.sprite("bean")]);
 
+const globals = {
+    player: addPlayer(game)
+}
+
 game.onClick(() => game.addKaboom(game.mousePos()));
-addPlayer(game)
 makeNPC(game)
